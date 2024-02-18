6484594f6da5120c95b7feec371f57825da65f42
DanielMEH <jdospina943@misena.edu.co>
2024-02-18 12:38:06 -0500

    path appp modified config app

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,39 @@
-import { Application } from "./app/app";
+import express from "express"
+import morgan from "morgan"
+import cors from "cors"
+import jsonapp from "./data/app.json"
+import { Logger } from "./utils/logs"
+import { routerInitialize } from "./routers/router"
+
+export class Application {
+    public AppServer: express.Application
+    constructor() {
+        this.AppServer = express()
+
+        this.AppServer.use(express.json())
+        this.config()
+        this.routes()
+        
+        this.AppServer.use(express.urlencoded({extended: true}))
+        this.AppServer.listen(jsonapp.Config_data.port, () => {
+            Logger().info(`Server running on: http://localhost:${jsonapp.Config_data.port}/${jsonapp.Config_data.schema}/${jsonapp.Config_data.path}`)
+           
+        })
+    }
+    private config(): void {
+        this.AppServer.use(cors())
+        this.AppServer.use(morgan("dev"))
+    }
+    private routes(): void {
+        this.AppServer.use("/",  routerInitialize.postLogin())
+        this.AppServer.use("/",  routerInitialize.postRegister())
+        this.AppServer.use("/",  routerInitialize.getProfile())
+        this.AppServer.use("/",  routerInitialize.putProfile())
+        this.AppServer.use("/",  routerInitialize.deleteProfile())
+        this.AppServer.use("/",  (req, res) => {
+            res.status(404).json({error: "Not found"})
+        })
+    }
+}
 
 new Application().AppServer
